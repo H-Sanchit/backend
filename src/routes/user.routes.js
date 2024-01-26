f0df460916de9c21dd5b8b053be0ea53a0da9d4c
Sanchit Hansrai <sanchith2369@gmail.com>
2024-01-26 14:37:27 +0530

    completed user controller and used pipelines

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  avatarUpdate,
+  changeCurrentPassword,
+  coverImageUpdate,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -29,6 +36,14 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), avatarUpdate);
+router
+  .route("/cover-image")
+  .patch(verifyJwt, upload.single("coverImage"), coverImageUpdate);
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
 router.route("/refresh-token").post(refreshAccessToken);
 export default router;
